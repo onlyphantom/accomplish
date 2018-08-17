6befbedfff4f3b7c7ef94548de3b357fe3291fbf
onlyphantom <samuel@outlook.sg>
2018-08-17 17:44:00 +0700

    Log In Form example

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template, redirect, url_for, session, request
+from functools import wraps
+import sqlite3
+
+DATABASE = 'accomplish.db'
+
 app = Flask(__name__)
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('Please log in first.')
+            return redirect(url_for('log'))
+    return wrap
 
 @app.route('/')
 def home():
@@ -8,6 +23,24 @@ def home():
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+
+@app.route('/log', methods=['POST', 'GET'])
+def log():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('welcome'))
+    return render_template('log.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('Successfully logged out.')
+    return redirect(url_for('log'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ notes.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2*np.pi, 1000)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.show()
+
+# Creating Decorator Class
+class decorator_class(object):
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print('Call Method executed before {}'.format(self.original_function.__name__))
+        return self.original_function(*args, **kwargs)
+
+@decorator_class
+def display_info(name, age):
+    print('Display function executed with arguments ({}, {})'.format(name, age))
+display_info("Megan", 21)
+
+# Creating Decorator Function
+def decorator_function(original):
+    def wrapper_function(*args, **kwargs):
+        print('Wrapper executed this before {}'.format(original.__name__))
+        return original(*args, **kwargs)
+    return wrapper_function
+
+# @decorator_function
+# def display_info_again(name, age):
+#     print('Display function executed with arguments ({}, {})'.format(name, age))
+
+# display_info_again("Sally", 41)
+
+from functools import wraps
+
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+        return orig_func(*args, **kwargs)
+    return wrapper
+
+def my_timer(orig_func):
+    import time
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        t2 = time.time()-t1
+        print('{} ran in {} sec'.format(orig_func.__name__, t2))
+        return(orig_func(*args, **kwargs))
+    return wrapper
+
+
+import time
+@my_logger
+@my_timer
+def display_info_again(name, age):
+    time.sleep(1)
+    print('The function executed with arguments ({}, {})'.format(name, age))
+
+display_info_again("Max", 33)
+
+
